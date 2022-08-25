3ed465b86c9cb0fd45b7aade9b8d194d608291a9
yu.hidaka <light3223b@gmail.com>
2022-08-25 21:00:55 +0900

    レスポンシブ対応

---
+++ script/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+$('.openbtn').click(function(){
+  $(this).toggleClass('active');
+  $('#g-nav').toggleClass('panelactive');
+});
+
+$('#g-nav a').click(function(){
+  $('.openbtn').removeClass('active');
+  $('#g-nav').removeClass('panelactive');
+});
